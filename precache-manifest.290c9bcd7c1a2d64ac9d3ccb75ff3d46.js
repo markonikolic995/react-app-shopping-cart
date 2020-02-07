aa45186bbad9acfa0427673381639836d1732712
unknown <marko.nikolic995@hotmail.com>
2020-02-07 02:35:17 +0100

    Updates

---
+++ precache-manifest.290c9bcd7c1a2d64ac9d3ccb75ff3d46.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef29ffa0b2431006db91594085842341",
+    "url": "/react-app-shopping-cart/index.html"
+  },
+  {
+    "revision": "4c2f1fd42e5b28e2be78",
+    "url": "/react-app-shopping-cart/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "56c4738973d47574242d",
+    "url": "/react-app-shopping-cart/static/css/main.98070e46.chunk.css"
+  },
+  {
+    "revision": "4c2f1fd42e5b28e2be78",
+    "url": "/react-app-shopping-cart/static/js/2.6821ac21.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-app-shopping-cart/static/js/2.6821ac21.chunk.js.LICENSE"
+  },
+  {
+    "revision": "56c4738973d47574242d",
+    "url": "/react-app-shopping-cart/static/js/main.87b0b2d6.chunk.js"
+  },
+  {
+    "revision": "b69f5afe59cb2671cd2a",
+    "url": "/react-app-shopping-cart/static/js/runtime-main.d543f188.js"
+  },
+  {
+    "revision": "9e404c7f5717dbb15acc85c122092daa",
+    "url": "/react-app-shopping-cart/static/media/backgroundIMG.9e404c7f.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-app-shopping-cart/static/media/logo.5d5d9eef.svg"
+  }
+]);
